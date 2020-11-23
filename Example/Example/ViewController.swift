5f092d7404923704fffc03b726a6c0776b15c3b5
Wiphu Blaine <Wiphu Blaine>
2020-11-23 22:17:09 +0700

    ok

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,45 +7,24 @@
 //
 
 import UIKit
-//import iLogin
-//import iHome
-//import iRating
-//import iEasy
 import iMusic
 
 class ViewController: UIViewController {
-    var login : iMusic?
-//    var home : iHome?
-//    var rating : iRating?
-//    var easy : iEasy?
-
-    var sView : UIScrollView?
+    var music : iMusic?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sView = UIScrollView(frame: CGRect.zero)
-        self.view.addSubview(sView!)
 
-        login = iLogin(frame: CGRect.zero)
-        sView?.addSubview(login!)
-
-        
+        music = iMusic(frame: CGRect.zero)
+        self.view.addSubview(music!)
     }
-    
-    
-    
 
-        override func viewWillLayoutSubviews() {
-            let w = self.view.frame.size.width;
-            let h = self.view.frame.size.height;
-            
-            sView?.frame = CGRect(x: w * 0.0, y: h * 0.0, width: w * 1.0, height: h * 1.0)
-            sView?.contentSize = CGSize(width: w * 1.0, height: h * 4.0)
-            
-
-            login?.frame = CGRect(x: w * 0.1, y: h * 1.5, width: w * 0.8, height: h * 0.3)
-             
-        }
+    override func viewWillLayoutSubviews() {
+        let w = self.view.frame.size.width;
+        let h = self.view.frame.size.height;
+        
+        music?.frame = CGRect(x: w * 0.0, y: h * 0.0, width: w * 1.0, height: h * 1.0)
+    }
 
 }
 
