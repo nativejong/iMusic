421bf7a13d457b6b01a337c56f4e992c9fce48d2
Wiphu Blaine <Wiphu Blaine>
2020-11-24 22:46:28 +0700

    ok

---
+++ Example/Example/VideoNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoNavigationController.swift
+//  Example
+//
+//  Created by John Blaine on 11/24/20.
+//  Copyright © 2020 John Blaine. All rights reserved.
+//
+
+import UIKit
+
+class VideoNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
